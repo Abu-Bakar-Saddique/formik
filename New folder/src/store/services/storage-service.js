de83ddf68b2e674786451b203caf75848db9c3fc
Abu Bakar Saddiuqe <144679988+Abu-Bakar-Saddique@users.noreply.github.com>
2023-11-27 23:02:17 -0800

    Delete New folder directory

--- New folder/src/store/services/storage-service.js
+++
@@ -1,8 +0,0 @@
-export const addItem = (value) => {
-    console.log(value);
-    localStorage.setItem("token", value['access_token']);
-  };
-  
-export const removeItem = (key) => {
-localStorage.removeItem(key);
-};
